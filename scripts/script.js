091f088564e341a809fd13b47352a265e275c179
angad trehan <84653725+angadtrehan@users.noreply.github.com>
2025-02-28 18:53:40 +0530

    Implement basic script file for stuff

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 console.log("Hello world!");
+
+const ctaButton = document.querySelector("button");
+ctaButton.addEventListener("click", () => {
+    const name = prompt("What is your name?");
+    const header = document.getElementById("header-logo");
+    const ctaText = document.getElementById("cta-text-title");
+    header.textContent =`Welcome, ${name}`;
+    ctaText.innerText = `Call to action! It's time, ${name}!`;
+});
